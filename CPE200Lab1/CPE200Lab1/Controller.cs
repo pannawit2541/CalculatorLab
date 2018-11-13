541111291c1b94ebca0660fe4e4e761def5a8d6c
PANNAWIT PANWONG <pannawit_panwong@cmu.ac.th>
2018-11-13 16:10:58 +0700

    Complete hw 5

---
+++ CPE200Lab1/CPE200Lab1/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class Controlller
+    {
+        CalculatorEngine Engine_1;
+        RPNCalculatorEngine Engine_2;
+        
+        public Controlller()
+        {
+            Engine_1 = new CalculatorEngine();
+            Engine_2 = new RPNCalculatorEngine();
+        }
+
+        public string Calculate(string str)
+        {
+            return Engine_1.Process(str);
+        }
+
+        public string RPNCalculate(string str)
+        {
+            return Engine_2.Process(str);
+        }
+    }
+}
